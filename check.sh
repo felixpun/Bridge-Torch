a0468306e63836955e49db9dc320e16db15763ec
Stardust-hyx <2216226605@qq.com>
2023-09-19 16:33:21 +0800

    first commit

---
+++ check.sh
@@ -0,0 +1,4 @@
+python src/check.py \
+    --checkpoint_path gc_bc_save/190000/mp_rank_00_model_states.pt \
+    --config_path gc_bc_save/config.json \
+    --traj_dir /data/hyx/raw/bridge_data_v2/deepthought_toykitchen1/many_skills/07/2023-04-04_13-21-42/raw/traj_group0/traj2
